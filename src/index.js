987bc84ec8e6ac789e84bcffc47080440a840be9
Faith Makinde <faithmakinde0@gmail.com>
2023-01-29 21:02:09 +0100

    about section completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './style.css'
-import Info from './Info.js'
-
+import Info from './Info'
+import About  from './About'
 function Index(){
   return (
     <div>
       <Info/> 
+      <About/>
     </div>
   )
 }
