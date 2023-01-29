16776fe8ab5bcc1a207232277b2701dea163b14b
Faith Makinde <faithmakinde0@gmail.com>
2023-01-29 20:40:09 +0100

    info section completed

--- src/Info.js
+++ src/Info.js
@@ -3,13 +3,13 @@ import pic from "./my portfolio pics.png";
 import icon from "./Icon (1).png"
 export default function Info(){
     return (
-        <header>
-            <img src={pic} alt='my-image'/>
-            <h2>Faith Makinde(f8_makinde)</h2>
-            <h3>Frontend Developer</h3>
-            <div>
-            <a href='#'><img src={icon} alt="" />Email</a>
-            <a href='#'><i class="fa-brands fa-linkedin"></i>Linkedln</a>
+        <header className='info--section'>
+            <img src={pic} alt='my-image' className='info--img'/>
+            <h2 className='info--name'>Faith Makinde(f8_makinde)</h2>
+            <h3 className='info--title'>Frontend Developer</h3>
+            <div className='info--flex'>
+            <a href='#' className='btn--1'><img src={icon} alt="" />&nbsp; Email</a>
+            <a href='#' className='btn--2'><i class="fa-brands fa-linkedin"></i>&nbsp; Linkedln</a>
             </div>
         </header>
     )
