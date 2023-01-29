85181fe070ede923b1afc1ba19db5ec37bfbe95e
Faith Makinde <faithmakinde0@gmail.com>
2023-01-29 21:17:17 +0100

    interests section completed

---
+++ src/Interests.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Interest(){
+    return(
+        <section className='interests--section'>
+            <h1 className='interests--bold'>Interest</h1>
+            <p className='interests--light'>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        </section>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom'
 import './style.css'
 import Info from './Info'
 import About  from './About'
+import Interest from './Interests'
 function Index(){
   return (
     <div>
-      <Info/> 
-      <About/>
+      <Info /> 
+      <About />
+      <Interest />
     </div>
   )
 }
